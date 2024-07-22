10cc9ef1f54bde752aba52286c858492960cac9c
Hector Ubaldo Vera San Roman <a00464819@gmail.com>
2024-07-22 14:10:32 -0600

    agregar las librerias para compose

--- app/src/main/java/com/integritas/demo/MainActivity.kt
+++ app/src/main/java/com/integritas/demo/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+//Id de cliente web: 57994672130-gp8gj8i2n2gs0raakqs7jr3okmng28u8.apps.googleusercontent.com
+//Id de cliente: 57994672130-ciut5u2vd2jvd06e0njd3pm3hr5502pj.apps.googleusercontent.com
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+
+        }
     }
 }
