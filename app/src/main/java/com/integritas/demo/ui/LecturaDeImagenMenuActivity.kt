650a77ab6da7785e2fd47c270c66069688e0a9f0
Hector Ubaldo Vera San Roman <a00464819@gmail.com>
2024-07-23 20:24:35 -0600

    Reconocimiento de texto en imagen

--- app/src/main/java/com/integritas/demo/ui/LecturaDeImagenMenuActivity.kt
+++ app/src/main/java/com/integritas/demo/ui/LecturaDeImagenMenuActivity.kt
@@ -6,16 +6,24 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.integritas.demo.databinding.ActivityLecturaDeImagenMenuBinding
 
 class LecturaDeImagenMenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLecturaDeImagenMenuBinding
+    private lateinit var textRecognizer: TextRecognizer
     private var uri: Uri? = null
     private val galeriaARL : ActivityResultLauncher<Intent> = obtenerGaleriaARL()
     private val camaraARL : ActivityResultLauncher<Intent> = obtenerCamaraARL()
@@ -23,6 +31,7 @@ class LecturaDeImagenMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLecturaDeImagenMenuBinding.inflate(layoutInflater)
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
         agregarListeners(binding)
         setContentView(binding.root)
     }
@@ -57,6 +66,29 @@ class LecturaDeImagenMenuActivity : AppCompatActivity() {
     }
 
     private fun reconocerTexto() {
+        if(uri == null) {
+            Toast.makeText(this, "Seleccionar o tomar foto del texto", Toast.LENGTH_SHORT).show()
+        } else {
+            reconocerTextoDeImagen()
+        }
+    }
+
+    private fun reconocerTextoDeImagen() {
+        try {
+            val inputImage = uri?.let { InputImage.fromFilePath(this, it) }
+            val textTask : Task<Text>? = inputImage?.let {
+                textRecognizer.process(it)
+                    .addOnSuccessListener { text ->
+                        val text = text.text
+                        binding.tvTextoReconocido.text = text
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("DemoInvestigacion", "reconocerTextoDeImagen: ${e.message}", )
+                    }
+            }
+        } catch (e:Exception) {
+            Log.e("DemoInvestigacion", "reconocerTextoDeImagen: ${e.message}", )
+        }
 
     }
 
@@ -68,6 +100,7 @@ class LecturaDeImagenMenuActivity : AppCompatActivity() {
                     val data = result.data
                     uri = data?.data
                     binding.ivImagen.setImageURI(uri)
+                    binding.tvTextoReconocido.text = ""
                 } else {
                     Toast.makeText(this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
                 }
@@ -81,6 +114,7 @@ class LecturaDeImagenMenuActivity : AppCompatActivity() {
             ActivityResultCallback { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
                     binding.ivImagen.setImageURI(uri)
+                    binding.tvTextoReconocido.text = ""
                 } else {
                     Toast.makeText(this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
                 }
