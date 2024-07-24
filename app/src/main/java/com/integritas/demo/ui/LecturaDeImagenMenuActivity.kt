6fc5168b7edf26b0a21dcc6a246b445cd750d756
Hector Ubaldo Vera San Roman <a00464819@gmail.com>
2024-07-23 19:36:40 -0600

    Seleccionar imagen desde galeria

--- app/src/main/java/com/integritas/demo/ui/LecturaDeImagenMenuActivity.kt
+++ app/src/main/java/com/integritas/demo/ui/LecturaDeImagenMenuActivity.kt
@@ -1,11 +1,32 @@
 package com.integritas.demo.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.integritas.demo.databinding.ActivityLecturaDeImagenMenuBinding
 
 class LecturaDeImagenMenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLecturaDeImagenMenuBinding
+    private var uri: Uri? = null
+    private val galeriaARL : ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                uri = data?.data
+                binding.ivImagen.setImageURI(uri)
+            } else {
+                Toast.makeText(this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLecturaDeImagenMenuBinding.inflate(layoutInflater)
@@ -14,7 +35,23 @@ class LecturaDeImagenMenuActivity : AppCompatActivity() {
     }
 
     private fun agregarListeners(binding : ActivityLecturaDeImagenMenuBinding) {
+        binding.btnAbrirGaleria.setOnClickListener {
+            abrirGaleria()
+        }
+        binding.btnAbrirCamara.setOnClickListener {
 
+        }
+        binding.btnReconocerTxt.setOnClickListener {
+
+        }
     }
+
+    private fun abrirGaleria() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.setType("image/*")
+        galeriaARL.launch(intent)
+    }
+
+
 }
 
