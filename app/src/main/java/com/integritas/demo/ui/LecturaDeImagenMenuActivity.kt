4164fe271d315f701c02df25ef91881b9d8f029d
Hector Ubaldo Vera San Roman <a00464819@gmail.com>
2024-07-23 20:03:26 -0600

    Captura de imagen por camara

--- app/src/main/java/com/integritas/demo/ui/LecturaDeImagenMenuActivity.kt
+++ app/src/main/java/com/integritas/demo/ui/LecturaDeImagenMenuActivity.kt
@@ -1,9 +1,11 @@
 package com.integritas.demo.ui
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
@@ -15,18 +17,9 @@ import com.integritas.demo.databinding.ActivityLecturaDeImagenMenuBinding
 class LecturaDeImagenMenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLecturaDeImagenMenuBinding
     private var uri: Uri? = null
-    private val galeriaARL : ActivityResultLauncher<Intent> = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult(),
-        ActivityResultCallback { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data = result.data
-                uri = data?.data
-                binding.ivImagen.setImageURI(uri)
-            } else {
-                Toast.makeText(this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
-            }
-        }
-    )
+    private val galeriaARL : ActivityResultLauncher<Intent> = obtenerGaleriaARL()
+    private val camaraARL : ActivityResultLauncher<Intent> = obtenerCamaraARL()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLecturaDeImagenMenuBinding.inflate(layoutInflater)
@@ -39,10 +32,10 @@ class LecturaDeImagenMenuActivity : AppCompatActivity() {
             abrirGaleria()
         }
         binding.btnAbrirCamara.setOnClickListener {
-
+            abrirCamara()
         }
         binding.btnReconocerTxt.setOnClickListener {
-
+            reconocerTexto()
         }
     }
 
@@ -52,6 +45,47 @@ class LecturaDeImagenMenuActivity : AppCompatActivity() {
         galeriaARL.launch(intent)
     }
 
+    private fun abrirCamara() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Titulo")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Descripcion")
 
+        uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        camaraARL.launch(intent)
+    }
+
+    private fun reconocerTexto() {
+
+    }
+
+    private fun obtenerGaleriaARL() : ActivityResultLauncher<Intent> {
+        return registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data = result.data
+                    uri = data?.data
+                    binding.ivImagen.setImageURI(uri)
+                } else {
+                    Toast.makeText(this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    private fun obtenerCamaraARL() : ActivityResultLauncher<Intent> {
+        return registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    binding.ivImagen.setImageURI(uri)
+                } else {
+                    Toast.makeText(this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
 }
 
